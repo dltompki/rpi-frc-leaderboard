475e67e766abc89b96be4d50ad58a191a892f91d
Dylan Tompkins <dylanltompkins@hotmail.com>
2021-04-01 22:33:36 -0400

    frc-api.py working

--- frc-api.py
+++ frc-api.py
@@ -1,14 +1,17 @@
 import requests
 import creds
+import base64
+
+authstring = creds.user + ':' + creds.passwd
+auth_bytes = authstring.encode('ascii')
+encoded_bytes = base64.b64encode(s=auth_bytes)
+encoded = encoded_bytes.decode('ascii')
+finalauth = 'Basic ' + encoded
 
 url = 'https://frc-api.firstinspires.org/v2.0/2021/rankings/IRHKR?teamNumber=1591'
-authstring = 'Basic ' + creds.user + ':' + creds.passwd
-headers = {
-    'Authorization': authstring,
-    'If-Modified-Since': ''
-}
+headers = {'Authorization': finalauth}
 
 response = requests.get(url, headers=headers)
 
 if __name__ == "__main__":
-    print(response.reason)
+    print(response.text)
